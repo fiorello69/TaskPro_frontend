34871f3655c4ab2b9cd631e246105b9dc1ad0d7e
Viorel Breban <vbreban69@gmail.com>
2024-07-21 22:21:30 +0200

    added project structure and loader, background images uploaded to cloudinary

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
+import { App } from 'App';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import 'modern-normalize';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.getElementById('root');
+
+createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter basename="/TaskPro_frontend">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
